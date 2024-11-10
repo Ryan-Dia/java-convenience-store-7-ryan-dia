da93dbd314c9f8be7c8590e4caf0bad3d7caaded
ryan-dia <cjfdnjs158@naver.com>
2024-11-10 20:42:19 +0900

    refactor: readFile 메서드를 더욱 간결하게 수정
    - 불필요한 컬렉션 선언 제거
    - 불필요한 변수(isFirstLine) 제거
    - bufferedReader만으로 구성

--- src/main/java/store/utils/MarkdownReader.java
+++ src/main/java/store/utils/MarkdownReader.java
@@ -3,26 +3,20 @@ package store.utils;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public final class MarkdownReader {
+    private static final String DELIMITER = ",";
+
     private MarkdownReader() {
     }
 
     public static List<String[]> readFile(String filePath) throws IOException {
-        List<String[]> data = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            boolean isFirstLine = true;
-            while ((line = br.readLine()) != null) {
-                if (isFirstLine) {
-                    isFirstLine = false;
-                    continue;
-                }
-                data.add(line.split(","));
-            }
+            return br.lines()
+                    .skip(1)
+                    .map(line -> line.split(DELIMITER))
+                    .toList();
         }
-        return data;
     }
 }
